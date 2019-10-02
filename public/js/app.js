1458703f9230b4d9af65f46cd5f9d0e30f9dadf8
spinetree <steve@spinetree.org>
2019-10-02 14:01:45 -0700

    jquery firing events on click of recents

--- public/js/app.js
+++ public/js/app.js
@@ -4,35 +4,41 @@ console.log('app.js reporting');
 
 $(document).ready(function () {
 
-  //   $('#favorites').append('derp');
-
+  // get favorites
   $.ajax({
     url: '/favorites/',
     type: 'get',
     dataType: 'html'
   })
     .then(favoritesList => {
-      console.log(favoritesList);
       $('#favorites').append(favoritesList);
     })
     .catch(error => {
       console.log('error: ' + error);
     });
 
+  // get recents
   $.ajax({
     url: '/search/',
     type: 'get',
     dataType: 'html'
   })
     .then(recentsList => {
-      console.log(recentsList);
+      // console.log(recentsList);
       $('#recents').append(recentsList);
+
+      $('.recent-search').on('click', function(event) {
+        event.preventDefault();
+        console.log(
+          event.target.text
+        );
+      })
     })
     .catch(error => {
       console.log('error: ' + error);
     });
 
-  return false;
 
+  return false;
 
 })
